ce88700f723d1e0380ba0d3a3959c118a2a08709
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-03-08 10:42:41 -0300

    Classe Position

---
+++ src/aplication/App.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Position pos = new Position(3, 4);
+
+        System.out.println(pos);
+
+    }
+}
