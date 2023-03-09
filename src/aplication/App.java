b915fa3ba3333669ea3a5684904ccc540d100951
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-03-09 13:48:25 -0300

    Creating board

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,13 +1,8 @@
 package aplication;
 
-import boardgame.Position;
-
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3, 4);
-
-        System.out.println(pos);
 
     }
 }
