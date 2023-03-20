76570e73cce930835d59c70817cee96decb22839
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-03-20 12:27:26 -0300

    added Chess Layer and printing board

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,5 +1,15 @@
 package aplication;
 
+import chess.ChessMatch;
+
 public class App {
     
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+
 }
