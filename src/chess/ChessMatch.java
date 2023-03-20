4deba3ed43ef7709f0077814114c4a689aca8eb0
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-03-20 13:12:01 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,8 +27,12 @@ public class ChessMatch {
 
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+    
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(5, 7));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
     }
     
 }
